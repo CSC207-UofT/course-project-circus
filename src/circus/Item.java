0a48f64c5be3f83b20d69c663ab97b70ca2e59e4
galacticglum <nerftactic09@gmail.com>
2021-10-07 21:44:03 -0400

    Created project and added Item class

---
+++ src/circus/Item.java
@@ -0,0 +1,58 @@
+package circus;
+
+import java.util.UUID;
+
+/**
+ * An item in the Inventory.
+ */
+public class Item {
+    private final UUID id;
+    private String name;
+    private String description;
+
+    /**
+     * Construct an Item with a name and description.
+     * @param name The name of this Item.
+     * @param description The description of this Item.
+     * @param id The UUID of this item.
+     */
+    public Item(String name, String description, UUID id) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    /**
+     * Construct an Item with a name, description, and random id.
+     * @param name The name of this Item.
+     * @param description The description of this Item.
+     */
+    public Item(String name, String description) {
+        this(name, description, UUID.randomUUID());
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
---
+++ src/circus/Main.java
@@ -0,0 +1,11 @@
+package circus;
+
+public class Main {
+    public static void main(String[] args) {
+        Item item1 = new Item("Mango", "A delectable fruit");
+        Item item2 = new Item("Crack Cocaine", "A children's candy");
+
+        System.out.println(item1);
+        System.out.println(item2);
+    }
+}
