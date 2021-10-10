68819b59136b2549340a8122184e7f8eeb17e2cc
Liam Bakar <20liamb@students.harker.org>
2021-10-10 18:49:25 -0400

    Created the pathfinder abstract class and added the first abstract method

---
+++ src/circus/Pathfinder.java
@@ -0,0 +1,18 @@
+package circus;
+import java.util.*;
+
+/**
+ * Abstract class for pathfinders that discovers the "optimal" path for packages.
+ *
+ * Classes that extend this one will have to implement the methods we choose here.
+ */
+public abstract class Pathfinder {
+
+    /**
+     * Returns the optimal path for packages. This method will differ based on the type of pathifnder we use.
+     *
+     * @return an array of strings representing the order of whcih packages it visits.
+     */
+    public abstract String[] findPath();
+
+}
