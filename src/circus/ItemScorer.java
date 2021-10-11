d6b7e36187d6d0a6db56f6d1c89675311ed7ed5c
Liam Bakar <20liamb@students.harker.org>
2021-10-11 15:30:52 -0400

    Created class ItemScorer
    Created the class ItemScorer but I'm not sure how we're going to be scoring. When we figure this out, we can implement the computeCost method and everything will come together.

---
+++ src/circus/ItemScorer.java
@@ -0,0 +1,15 @@
+package circus;
+
+public class ItemScorer implements Scorer<Item> {
+
+    /**
+     * Computes the cost of two items (the distance between two items) using some formula we come up with.
+     * @param from The first item
+     * @param to The second item
+     * @return The cost of two items
+     */
+    @Override
+    public double computeCost(Item from, Item to) {
+        return 0;
+    }
+}
