bcd00902f113dbbb927e0476973dcb888a74e1f3
galacticglum <nerftactic09@gmail.com>
2021-10-15 13:04:24 -0400

    Fix project style errors

--- src/circus/pathfinding/Scorer.java
+++ src/circus/pathfinding/Scorer.java
@@ -3,5 +3,5 @@ package circus.pathfinding;
 import circus.inventory.Item;
 
 public interface Scorer<T extends Item> {
-    public double computeCost(T from, T to);
+    double computeCost(T from, T to);
 }
