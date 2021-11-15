4692d888995f38b5ddfbd7dde0325ddecfc6266e
galacticglum <nerftactic09@gmail.com>
2021-11-15 12:04:19 -0500

    Converted ItemReceivedMessageData to class

--- src/main/java/warehouse/ItemReceivedMessageData.java
+++ src/main/java/warehouse/ItemReceivedMessageData.java
@@ -5,13 +5,26 @@ import inventory.Item;
 /**
  * Data for the onItemReceived message.
  */
-public record ItemReceivedMessageData(Item item, Receivable receivable) {
+public class ItemReceivedMessageData {
+    private final Item item;
+    private final Receivable receivable;
+
     /**
      * Construct ItemReceivedMessageData given an Item and Receivable.
      *
      * @param item The Item that was received.
      * @param receivable The Receivable that was modified.
      */
-    public ItemReceivedMessageData {
+    public ItemReceivedMessageData(Item item, Receivable receivable) {
+        this.item = item;
+        this.receivable = receivable;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public Receivable getReceivable() {
+        return receivable;
     }
 }
