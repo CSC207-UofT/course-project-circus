3e52e2fc45a7270d85dde907031699ddc6f2d67f
fionyvan <fiony.van@mail.utoronto.ca>
2021-12-05 12:37:46 -0500

    Tested all classes in Item.java

--- src/tests/java/ItemTest.java
+++ src/tests/java/ItemTest.java
@@ -10,12 +10,38 @@ public class ItemTest {
      * Testing getId with all parameters specified
      */
     @Test
-    public void getIdTest(){
+    void getId(){
         Part part = new Part("1", "Empty", "An Empty Part");
         Item item = new Item(part.getId(), part);
-        assertEquals(item.getId().toString(), "1");
+        assertEquals("1", item.getId());
     }
 
+    /**
+     * Testing getId with randomized Id
+     */
+    @Test
+    void getIdRandomized(){
+        Part part = new Part("Empty", "An Empty Part");
+        Item item = new Item(part);
+        String id = item.getId();
+        assertEquals(id, item.getId());
+    }
 
+    /**
+     * Testing getPart with all parameters specified
+     */
+    @Test
+    void getPart() {
+        Part part = new Part("1", "Empty", "An Empty Part");
+        Item item = new Item(part.getId(), part);
+        assertEquals(part, item.getPart());
+    }
 
+    @Test
+    void testToString() {
+        Part part = new Part("1", "Empty", "An Empty Part");
+        Item item = new Item(part.getId(), part);
+        assertEquals("Part{id=1, name='Empty', description='An Empty Part'}", part.toString());
+
+    }
 }
