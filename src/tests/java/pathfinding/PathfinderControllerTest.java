0376a9285ac86c24d88630f1681cc82a5b5e4ddf
galacticglum <nerftactic09@gmail.com>
2021-12-05 22:00:54 -0500

    Updated PathfinderControllerTest

--- src/tests/java/pathfinding/PathfinderControllerTest.java
+++ src/tests/java/pathfinding/PathfinderControllerTest.java
@@ -13,12 +13,13 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * Test the Pathfinder class.
  */
-public class PathfinderTest {
+public class PathfinderControllerTest {
     /**
-     *
+     * Test the getPath method.
      */
     @Test
     public void testGetPath() {
+        // Build the warehouse
         Warehouse warehouse = new Warehouse(10, 10);
         warehouse.setTile(new Rack(0, 1, 5));
         warehouse.setTile(new Rack(1, 1, 5));
@@ -30,20 +31,37 @@ public class PathfinderTest {
         warehouse.setTile(new Rack(2, 4, 5));
         warehouse.setTile(new Rack(5, 4, 5));
         warehouse.setTile(new Rack(4, 5, 5));
-
-        PathfinderController check = new PathfinderController(
+        // Build the pathfinder
+        PathfinderController pathfinder = new PathfinderController(
                 warehouse.getTiles(), warehouse.getTileAt(0, 0),
                 warehouse.getTileAt(6, 7));
-        int [][] expected = {{0, 0}, {1, 0}, {2, 0}, {3, 0}, {3, 1}, {3, 2}, {3, 3}, {3, 4}, {3,5}, {3, 6}, {3, 7},
-                {4, 7}, {5, 7}, {6, 7}};
+
+        // The expected path, stored as a 2D array of tile positions.
+        int [][] expected = {
+                {0, 0},
+                {1, 0},
+                {2, 0},
+                {3, 0},
+                {3, 1},
+                {3, 2},
+                {3, 3},
+                {3, 4},
+                {3,5},
+                {3, 6},
+                {3, 7},
+                {4, 7},
+                {5, 7},
+                {6, 7}
+        };
+
         int i = 0;
-        ArrayList<Tile> path = check.getPath();
-        for(Tile p: path)
+        ArrayList<Tile> path = pathfinder.getPath();
+        // Make sure each tile in the path matches.
+        for(Tile p : path)
         {
             assertEquals(expected[i][0], p.getX());
             assertEquals(expected[i][1], p.getY());
             i++;
         }
-
     }
 }
