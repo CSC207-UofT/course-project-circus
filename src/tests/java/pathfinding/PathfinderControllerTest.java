2ad0b6149bfd0dfd2dbe38dfb533b5a906ddbba0
galacticglum <nerftactic09@gmail.com>
2021-12-05 22:04:54 -0500

    Staging

--- src/tests/java/pathfinding/PathfinderControllerTest.java
+++ src/tests/java/pathfinding/PathfinderControllerTest.java
@@ -46,7 +46,7 @@ public class PathfinderControllerTest {
                 {3, 2},
                 {3, 3},
                 {3, 4},
-                {3,5},
+                {3, 5},
                 {3, 6},
                 {3, 7},
                 {4, 7},
